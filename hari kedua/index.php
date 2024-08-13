6196695e64d11e25c9b27c820a72f6433c311273
apipapip <hafiefmuhammad@gmail.com>
2024-08-13 14:01:23 +0700

    penambahan kode untuk php

--- hari kedua/index.php
+++ hari kedua/index.php
@@ -6,8 +6,55 @@ const phi=3.14;
 echo $luas;
 echo "<br>";
 echo phi;
-echo "<br>hari kedua belajar bahasa pemrograman php";
-print "<br>hari ini tanpa kendala dan aman";
+
+$luasPersegiPanjang=67;
+$kelilingPersegiPanjang=93.5;
+$nama="apiip";
+$hidup=true;
 echo "<br>";
-$luasPersegiPanjang=9;
-echo $luasPersegiPanjang;
+
+var_dump($luasPersegiPanjang);
+var_dump($kelilingPersegiPanjang);
+var_dump($nama);
+var_dump($hidup);
+
+$x = 5;
+var_dump($x);
+echo "<br>";
+$x="hello world";
+$y='halo dunia';
+
+var_dump($x);
+echo "<br>";
+var_dump($y);
+
+echo "<br>";
+$x=10.365;
+var_dump($x);
+
+echo "<br>";
+$x = 5985;
+var_dump($x); 
+
+print "<h2>example</h2>";
+
+echo "<br>";
+$cars = array("Volvo","BMW","Toyota");
+var_dump($cars);
+
+echo "<br>";
+class Car {
+    public $color;
+    public $model;
+    public function __construct($color, $model) {
+      $this->color = $color;
+      $this->model = $model;
+    }
+    public function message() {
+      return "My car is a " . $this->color . " " . $this->model . "!";
+    }
+  }
+  
+  $myCar = new Car("red", "Volvo");
+  var_dump($myCar);
+  
