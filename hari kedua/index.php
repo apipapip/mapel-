b3821de582e64fa266c8bc6bc07b767f524fda89
apipapip <hafiefmuhammad@gmail.com>
2024-08-13 09:20:38 +0700

    menambah file

---
+++ hari kedua/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$luas=7;
+const phi=3.14;
+ 
+echo $luas;
+echo "<br>";
+echo phi;
+echo "<br>hari kedua belajar bahasa pemrograman php";
+print "<br>hari ini tanpa kendala dan aman";
+echo "<br>";
+$luasPersegiPanjang=9;
+echo $luasPersegiPanjang;
